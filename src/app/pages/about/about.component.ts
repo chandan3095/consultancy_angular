961ace43f687f7ff7196eda5581b6f7f4d98afdd
chandan3095 <chandanghosh593@gmail.com>
2025-01-10 17:30:41 +0530

    about page compete

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SectionTitleComponent } from '../../shared-components/section-title/section-title.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [SectionTitleComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
